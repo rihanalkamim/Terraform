b58278a5c19460f764a620224141c2a424d63dce
rihan <rihanalkamim@gmail.com>
2023-11-21 01:03:46 -0300

    Add AMI and Instance_Type

--- main.tf
+++ main.tf
@@ -20,6 +20,9 @@ resource "docker_image" "nginx" {
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
   name  = "test_docker_terraform"
+  ami = "ami-830c94e3"
+  instance_type = "t2.micro"
+
 
   ports {
     internal = 80
