040d24a8d9b397f76f67778744d987e8b92cf92a
rihan <rihanalkamim@gmail.com>
2023-11-21 01:20:12 -0300

    Change resource

--- instance.tf
+++ instance.tf
@@ -14,7 +14,7 @@ provider "aws" {
     region = "us-west-2"
 }
 
-resource "aws_primary_instance" "app-server" {
+resource "aws_instance" "app-server" {
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
 
